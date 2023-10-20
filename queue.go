fcd1503eee0a4e67ebf41ca6e61c866b7128de69
Kristian Veter <rscrafter1234@icloud.com>
2023-10-20 15:55:35 +0300

    create queue actions

--- queue.go
+++ queue.go
@@ -4,3 +4,33 @@ package main
 // get from queue
 // delete from queue
 //  queue clean
+
+func (v *VoiceInstance) GetQueueSong() (song Song) {
+	v.queueMutex.Lock()
+	defer v.queueMutex.Unlock()
+	if len(v.queue) != 0 {
+		return v.queue[0]
+	}
+	return
+}
+
+func (v *VoiceInstance) AddQueue(song Song) {
+	v.queueMutex.Lock()
+	defer v.queueMutex.Unlock()
+
+	v.queue = append(v.queue, song)
+}
+
+func (v *VoiceInstance) QueueRemoveFisrt() {
+	v.queueMutex.Lock()
+	defer v.queueMutex.Unlock()
+	if len(v.queue) != 0 {
+		v.queue = v.queue[1:]
+	}
+}
+
+func (v *VoiceInstance) QueueClean() {
+	v.queueMutex.Lock()
+	defer v.queueMutex.Unlock()
+	v.queue = []Song{}
+}
